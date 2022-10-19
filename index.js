5d98581ab24483f6f9814b286db493c25bf419bd
Prastowo Dwi Atmoko <prastowo.atmoko@corp.angkasapura2.co.id>
2022-10-19 18:30:31 +0700

    update index.js

--- index.js
+++ index.js
@@ -6,7 +6,6 @@ const bannerRouter = require("./routes/BannerRouter");
 
 require("dotenv").config();
 
-<<<<<<< HEAD
 global.__basedir = __dirname;
 
 app.use(bodyParser.json());
@@ -16,13 +15,6 @@ app.use(cartRouter, bannerRouter);
 
 app.get("/", (req, res) => {
   res.send("Hello World");
-=======
-app.use(express.json());
-app.use(cartRouter);
-
-app.get('/', (req, res) => {
-    res.send('Hello World');
->>>>>>> 7761f1fcbb3e49e14a6e319e497dfc803d8eba75
 });
 
 app.listen(process.env.PORT, () => console.log(`Server Running at ${process.env.PORT}`));
